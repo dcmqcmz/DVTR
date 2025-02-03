85401d5f2fbd761abe40d2201f67bb1eef198fd7
dcmqcmz <castillovazquez111@gmail.com>
2025-02-02 18:34:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,44 +7,43 @@ const video2 = document.getElementById('video2');
 const video3 = document.getElementById('video3');
 
 const noMessages = [
-  "¿En serio no? 😱 ¡Lo intentaré más tarde!",
-  "¡Ay, qué triste! 😭",
-  "¡Te estoy esperando! 😘",
-  "¿No me amas? 🥺 ¡Lo intentaré de nuevo!",
-  "¡No puedes decirme que no! 💔😔"
+    "¿En serio no? 😱 ¡Lo intentaré más tarde!",
+    "¡Ay, qué triste! 😭",
+    "¡Te estoy esperando! 😘",
+    "¿No me amas? 🥺 ¡Lo intentaré de nuevo!",
+    "¡No puedes decirme que no! 💔😔"
 ];
 
 yesButton.addEventListener('click', () => {
-  // Cambiar el tamaño de los botones
-  yesButton.classList.add('clicked');
-  noButton.classList.add('clicked');
+    // Cambiar el tamaño de los botones
+    yesButton.classList.add('clicked');
+    noButton.classList.add('clicked');
 
-  // Mensaje de aceptación
-  messageDiv.innerHTML = '¡Sabía que dirías que sí! Te amo mucho, amor 👻😍💗';
+    // Mensaje de aceptación
+    messageDiv.innerHTML = '¡Sabía que dirías que sí! Te amo mucho, amor 👻😍💗';
 
-  // Los vídeos siguen siendo visibles
-  videoContainer.style.display = 'flex';
-
-  // Reproducir los vídeos
-  video1.style.display = 'block';
-  video2.style.display = 'block';
-  video3.style.display = 'block';
+    // Mostrar vídeos
+    videoContainer.style.display = 'flex';
+    video1.style.display = 'block';
+    video2.style.display = 'block';
+    video3.style.display = 'block';
 });
 
 noButton.addEventListener('click', () => {
-  // Seleccionar un mensaje aleatorio de las opciones disponibles
-  const randomMessage = noMessages[Math.floor(Math.random() * noMessages.length)];
+    // Seleccionar un mensaje aleatorio de las opciones disponibles
+    const randomMessage = noMessages[Math.floor(Math.random() * noMessages.length)];
 
-  // Mostrar un mensaje de rechazo aleatorio
-  messageDiv.innerHTML = randomMessage;
+    // Mostrar un mensaje de rechazo aleatorio
+    messageDiv.innerHTML = randomMessage;
 
-  // Los vídeos siguen siendo visibles, solo que no cambiará el mensaje
-  videoContainer.style.display = 'flex'; // Mantener el contenedor de vídeos visible
-  video1.style.display = 'block';
-  video2.style.display = 'block';
-  video3.style.display = 'block';
+    // Los vídeos siguen siendo visibles
+    videoContainer.style.display = 'flex'; // Mantener el contenedor de vídeos visible
+    video1.style.display = 'block';
+    video2.style.display = 'block';
+    video3.style.display = 'block';
 
-  // Cambiar el tamaño de los botones
-  noButton.classList.add('clicked');
-  yesButton.classList.remove('clicked');
+    // Cambiar el tamaño de los botones
+    noButton.classList.add('clicked');
+    yesButton.classList.remove('clicked');
 });
+
